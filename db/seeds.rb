821d28bda7b52dabce46f05b9128065652862550
isangidien <i.inessa.miller@gmail.com>
2017-06-02 17:53:53 -0400

    checkpoint-28-authorization complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,11 +37,21 @@ topics = Topic.all
      body: RandomData.random_paragraph
    )
  end
- user = User.first
-  user.update_attributes!(
-    email: 'i.inessa.miller@gmail.com',
-    password: 'helloworld'
-  )
+
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
  puts "Seed finished"
  puts "#{User.count} users created"
