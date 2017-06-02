b2703a86803affb5ab4fa44274ab3fdc6d462e7f
isangidien <i.inessa.miller@gmail.com>
2017-06-02 19:41:17 -0400

    checkpoint 29 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  has_many :comments
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
 
@@ -16,5 +17,5 @@ class User < ActiveRecord::Base
    has_secure_password
 
    enum role: [:member, :admin]
-   
+
 end
